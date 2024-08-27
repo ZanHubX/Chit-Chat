778f4839e4d53243f617d3b18c3473c420188ea3
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-08-27 18:28:19 +0630

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import zhangImage from "../assets/zhang.jpg";
+import zhangImage from "../assets/Zhang.jpg";
 import AquaChat from "../assets/AquaChat.png";
 
 
