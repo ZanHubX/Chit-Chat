c501d17fbe507195928be90cf97f03db91c2556d
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-09-20 17:07:34 +0630

    last commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,6 +41,7 @@ const Login = () => {
               trigger="hover"
               style={{ width: "25px", height: "25px" }}
             ></lord-icon>
+            
           </span>
           <input
             type="text"
@@ -48,6 +49,7 @@ const Login = () => {
             className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Email Address"
           />
+          
         </div>
 
         <div className="flex mt-5">
