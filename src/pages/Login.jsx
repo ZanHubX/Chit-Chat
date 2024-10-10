2d6026665f9e34daacc5e5236fff593d3db17614
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-10-10 12:58:29 +0630

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
             <lord-icon
               src="https://cdn.lordicon.com/nzixoeyk.json"
               trigger="hover"
-              style={{ width: "20px", height: "25px" }}
+              style={{ width: "20px", height: "20px" }}
             ></lord-icon>
             
           </span>
