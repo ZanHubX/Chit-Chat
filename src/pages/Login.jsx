d154c81ed42844a47ba39929e55da4351498928d
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-10-21 16:50:46 +0700

    last changes code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
           <input
             type="text"
             id="website-admin"
-            className="rounded-none rounded-e-lg bg-gray-400 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Email Address"
           />
           
